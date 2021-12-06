7206702b18db024f76a73b11b29c0f60a35dd972
TheoD <denoyeletheo@gmail.com>
2021-12-06 08:01:21 +0100

    feat: get/send HTML tables with ranking and days

---
+++ index.ts
@@ -0,0 +1,73 @@
+import express from 'express';
+import jsdom from 'jsdom';
+
+const { JSDOM } = jsdom;
+
+const app = express();
+
+const port : number = 3000;
+
+app.all("/", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+
+  return next();
+});
+
+app.get('/', (req, res ) => {
+  if (req.query.saison == undefined || req.query.codent == undefined || req.query.poule == undefined) {
+    res.send(
+    {
+    'error': {
+      'code': 500,
+      'message': 'Missing argument in the request, be sure it contain the arguments: saison , codent , poule'
+      }
+    })
+  } else {
+    const url = getFFVBurl(req.query)
+    sendHTMLtables(url)
+    .then( (tables : HTMLCollectionOf<HTMLTableElement> ) => {
+      res.send(tables[0].outerHTML + tables[1].outerHTML )
+    })
+  }
+});
+
+function getFFVBurl(query) {
+  return "https://www.ffvbbeach.org/ffvbapp/resu/vbspo_calendrier.php?saison=" + query.saison + "&codent=" + query.codent + "&poule=" + query.poule
+}
+
+function sendHTMLtables(url: string) {
+  return new Promise( (resolve,reject) => {
+
+    var resourceLoader = new jsdom.ResourceLoader({
+      strictSSL: false,
+  });
+
+  var options = { 
+      resources: resourceLoader,
+  };
+
+  jsdom.JSDOM.fromURL(url, options) 
+    .then(dom => {
+      let tables = dom.window.document.querySelectorAll("table")
+      resolve( [ tables[2], tables[3] ] ) // [ ranking, days ]
+    })
+    .catch((error => {
+      console.error(error)
+      reject({
+        'error': {
+          'code': 0,
+          'message': 'rejected'
+          }
+        })
+    }))
+  })
+}
+
+app.listen(port, () => {
+  return console.log(`server is listening on ${port}`);
+});
+
+
+
+
